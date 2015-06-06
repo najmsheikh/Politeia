01a04c014a643f073bf0baac800c91db8f1a8f68
Najm Sheikh <najm97@gmail.com>
2015-06-06 14:32:53 -0400

    Added ability to take image requests

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+var express = require('express');
+var bodyParser = require('body-parser');
 var rekognition = require('rekognition');
 var nameDB = require('./fire.js');
 var OpenSecretsClient = require('opensecrets');
@@ -8,25 +10,36 @@ rekognition.config({
     api_secret: "sgqKjcfFfNsHk4A9"
 });
 
-var image = 'http://i.huffpost.com/gen/1082266/images/o-STEVE-COHEN-TWITTER-facebook.jpg';
+var app = express();
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 
-rekognition.faceDetect(image, function(err, body) {
-    if (err)
-        throw err;
-    if (!err) {
-        var nameList = JSON.parse(body).face_detection[0].name;
-        var name = nameList.substring(0, nameList.indexOf(':'));
-        name = name.replace('_', ' ');
-        nameDB(name, function(cid) {
-            client.candContrib(cid, function(err, json) {
-                if (err) throw err;
-                console.log('--------------'+name+'\'s Contributions------------');
-                for (var i = 0; i < json['contributor'].length; i++) {
-                    console.log(json['contributor'][i]['@attributes'].org_name);
-                };
-                process.exit();
+// var image = 'http://i.huffpost.com/gen/1082266/images/o-STEVE-COHEN-TWITTER-facebook.jpg';
+
+app.post('/lookup', function(req, res) {
+    var image = req.body.image;
+    rekognition.faceDetect(image, function(err, body) {
+        if (err)
+            throw err;
+        if (!err) {
+            var nameList = JSON.parse(body).face_detection[0].name;
+            var name = nameList.substring(0, nameList.indexOf(':'));
+            name = name.replace('_', ' ');
+            nameDB(name, function(cid) {
+                client.candContrib(cid, function(err, json) {
+                    if (err) throw err;
+                    console.log('--------------' + name + '\'s Contributions------------');
+                    for (var i = 0; i < json['contributor'].length; i++) {
+                        console.log(json['contributor'][i]['@attributes'].org_name);
+                    };
+                });
             });
-        });
-    };
+        };
+    });
+    res.end();
 });
 
+app.listen(process.env.PORT, function() {
+    console.log("Server is up  running at port: " + process.env.PORT);
+});
