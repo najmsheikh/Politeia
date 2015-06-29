9d0e13b8d8d5c45c5d9e3d447f4e6fd3f99be9c0
Najm Sheikh <najm97@gmail.com>
2015-06-29 19:48:57 -0400

    Index, enroll and recognize

---
+++ kairos.js
@@ -0,0 +1,35 @@
+var request = require('request');
+
+module.exports.enroll = function(json) {
+    request({
+        method: 'POST',
+        url: 'https://api.kairos.com/enroll',
+        json: true,
+        headers: {
+            'Content-Type': 'application/json',
+            'app_id': '011ab8ee',
+            'app_key': '31b0f5505b524cc52ad17b8ca3cb312d'
+        },
+        body: json
+    }, function(error, response, body) {
+        if (body.images[0].transaction.status == 'success')
+            console.log(json.subject_id + ' was enrolled.');
+    });
+};
+
+module.exports.recognize = function(json, callback) {
+    request({
+        method: 'POST',
+        url: 'https://api.kairos.com/recognize',
+        json: true,
+        headers: {
+            'Content-Type': 'application/json',
+            'app_id': '011ab8ee',
+            'app_key': '31b0f5505b524cc52ad17b8ca3cb312d'
+        },
+        body: json
+    }, function(error, response, body) {
+        if (body.images[0].transaction.status == 'success')
+        	callback(body.images[0].transaction.subject);
+    });
+}
--- server.js
+++ server.js
@@ -1,44 +1,73 @@
 var express = require('express');
+var kairos = require('./kairos.js');
 var bodyParser = require('body-parser');
-var rekognition = require('rekognition');
 var nameDB = require('./fire.js');
 var OpenSecretsClient = require('opensecrets');
 var client = new OpenSecretsClient('a2810bc194384a049f6a7fb646d897ad');
-
-rekognition.config({
-    api_key: "yRaOk6T9dONHHzvw",
-    api_secret: "sgqKjcfFfNsHk4A9"
-});
 
 var app = express();
 app.use(bodyParser.urlencoded({
     extended: false
 }));
 
+app.get('/', function(req, res) {
+    res.sendfile("./index.html");
+});
+app.get(/^(.+)$/, function(req, res) {
+    res.sendfile(__dirname + req.params[0]);
+});
+
 // var image = 'http://i.huffpost.com/gen/1082266/images/o-STEVE-COHEN-TWITTER-facebook.jpg';
 
-app.post('/lookup', function(req, res) {
-    var image = req.body.image;
-    rekognition.faceDetect(image, function(err, body) {
-        if (err)
-            throw err;
-        if (!err) {
-            var nameList = JSON.parse(body).face_detection[0].name;
-            var name = nameList.substring(0, nameList.indexOf(':'));
-            name = name.replace('_', ' ');
-            nameDB(name, function(cid) {
-                client.candContrib(cid, function(err, json) {
-                    if (err) throw err;
-                    console.log('--------------' + name + '\'s Contributions------------');
-                    for (var i = 0; i < json['contributor'].length; i++) {
-                        console.log(json['contributor'][i]['@attributes'].org_name);
-                    };
-                });
-            });
+// app.post('/lookup', function(req, res) {
+//     var image = req.body.image;
+//     rekognition.faceDetect(image, function(err, body) {
+//         if (err)
+//             throw err;
+//         if (!err) {
+//             var nameList = JSON.parse(body).face_detection[0].name;
+//             var name = nameList.substring(0, nameList.indexOf(':'));
+//             name = name.replace('_', ' ');
+//             nameDB(name, function(cid) {
+//                 client.candContrib(cid, function(err, json) {
+//                     if (err) throw err;
+//                     console.log('--------------' + name + '\'s Contributions------------');
+//                     for (var i = 0; i < json['contributor'].length; i++) {
+//                         console.log(json['contributor'][i]['@attributes'].org_name);
+//                     };
+//                 });
+//             });
+//         };
+//     });
+//     res.end();
+// });
+
+app.post('/enroll', function(req, res) {
+    var img_url = req.body.img_url;
+    var politician = req.body.politician;
+    var json = {
+            image: img_url,
+            subject_id: politician,
+            gallery_name: 'politica'
         };
-    });
+    kairos.enroll(json);
     res.end();
 });
+
+app.post('/recognize', function(req, res){
+    var img_url = req.body.img_url;
+    var accuracy = req.body.threshold;
+    var json = {
+            url: img_url,
+            gallery_name: 'politica',
+            threshold: accuracy,
+            max_num_results: 1
+        };
+    kairos.recognize(json, function(data){
+        console.log(data);
+    });
+    res.end();
+})
 
 app.listen(process.env.PORT, function() {
     console.log("Server is up  running at port: " + process.env.PORT);
