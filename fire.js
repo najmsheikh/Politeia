e601e860bf101ec695187b874f44fa9dfb116965
Najm Sheikh <najm97@gmail.com>
2015-06-29 22:41:36 -0400

    Added lookup function

--- fire.js
+++ fire.js
@@ -1,16 +1,20 @@
 var firebase = require('firebase');
 var fb = new firebase('https://politica.firebaseio.com/');
 
-module.exports = function(word, callback) {
-  fb.once('value', function(snapshot) {
-      var data = snapshot.val();
-      data.forEach(function(dataSnap) {
-          var index = word.indexOf(' ');
-          var first = dataSnap.Name.substring(0, index);
-          var last = word.substring(index + 1);
-          var candidate = dataSnap.Name;
-          if (candidate.indexOf(first) >= 0 && candidate.indexOf(last) >= 0)
-            callback(dataSnap.CID);
-      });
-  });
+module.exports = function(politician, callback) {
+    fb.once('value', function(snapshot) {
+        var data = snapshot.val();
+        data.forEach(function(dataSnap) {
+            var index = politician.indexOf(' ');
+            var first = politician.substring(0,index).capitalize();
+            var last = politician.substring(index+1).capitalize();
+            var candidate = dataSnap.Name;
+            if (candidate.indexOf(last) >= 0 && candidate.indexOf(first) >= 0)
+                callback(dataSnap.CID);
+        });
+    });
+};
+
+String.prototype.capitalize = function() {
+    return this.charAt(0).toUpperCase() + this.slice(1);
 }
